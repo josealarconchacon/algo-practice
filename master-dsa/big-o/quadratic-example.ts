469214c2dc34d3e025c5b712d5f00ee11aa35543
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-07-19 12:22:34 -0400

    Log all numbers in array, then their sum

--- master-dsa/big-o/quadratic-example.ts
+++ master-dsa/big-o/quadratic-example.ts
@@ -11,3 +11,20 @@ function logAllPairs(nums: number[]): void {
 
 const exampleArray = [1, 2, 3, 4];
 logAllPairs(exampleArray); // O(n^2)
+
+// Log all numbers in array, then their sum
+function logNumberAndTheirSum(nums: number[]): void {
+    console.log('All numbers')
+    nums.forEach(n => { // O(n)
+        console.log(n)
+    })
+
+    console.log('Sum of all numners')
+    nums.forEach(firstNum => { // O(^2)
+        nums.forEach(secondNum => {
+            console.log(firstNum + secondNum)
+        })
+    })
+}
+// O(n + n^2) = O(n^2)
+console.log(logNumberAndTheirSum([1,2,3]))
